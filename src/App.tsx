9ef4b1219cdd09eb2ac33a108c6a9b2380588df0
vihanpereraux <vihanpereraux@gmail.com>
2025-01-17 12:19:39 +0000

    add route layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import ChatRoom from './pages/ChatRoom'
+
+// stylesheet
 import './App.css'
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' Component={Home}></Route>
+          <Route path='/chat-room' Component={ChatRoom}> </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/ChatRoom.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatRoom: React.FC = () => {
+    return(
+        <>
+            Chat Room
+        </>
+    )
+}
+
+export default ChatRoom
