1816d2babe837146728efae4e5fbf61e5ab3b5f3
vihanpereraux <vihanpereraux@gmail.com>
2025-01-18 09:17:18 +0000

    update register comp.

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 // firebase config
-import { collection, addDoc } from "firebase/firestore";
+import { collection, addDoc, getDocs } from "firebase/firestore";
 import { getFirebaseConfig } from "../config/FirebaseConfig";
 const db = getFirebaseConfig();
 
 // props
-import { RegisterAuthProps } from "../interfaces/props";
+import { RegisterAuthProps, LoginErrorProps } from "../interfaces/props";
 
 const Register: React.FC = () => {
-
     let detailsSnaphot = { devicename: "", username: "", password: "" }
-    const [formDetails, setFormDetails] = useState<RegisterAuthProps>(detailsSnaphot)
+    const [formDetails, setFormDetails] = useState<RegisterAuthProps>(detailsSnaphot);
+
+    let errorSnapshot: LoginErrorProps = { isError: false, error: "no_error" }
+    const [error, setError] = useState<LoginErrorProps>(errorSnapshot)
+
+    const navigate = useNavigate()
 
     const handleDetails = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.id === "device_name") {
@@ -29,12 +34,34 @@ const Register: React.FC = () => {
     }
 
     const handleSubmit = async () => {
-        let newItem: RegisterAuthProps = formDetails
-        try {
-            await addDoc(collection(db, "my_devices"), { ...newItem })
-            console.log(`Document is written to the db`)
-        } catch (error) {
-            console.log(`Error occured - ${error}`)
+        // check existing records
+        const querySnaphot = await getDocs(collection(db, "my_devices"));
+        const items: any[] = querySnaphot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+
+
+        console.log(items.some(item => (item.username === formDetails.username)))
+        console.log(items.some(item => (item.devicename === formDetails.devicename)))
+
+        const isUsernameFound = items.some(item => (item.username === formDetails.username))
+        const isDevicenameFound = items.some(item => (item.devicename === formDetails.devicename))
+
+        if (isUsernameFound || isDevicenameFound) {
+            errorSnapshot.isError = true;
+            errorSnapshot.error = "user already existing in the system";
+            setError({ ...errorSnapshot });
+            console.log('same user found');
+        }
+        else {
+            console.log(formDetails)
+            try {
+                let newItem: RegisterAuthProps = formDetails
+                await addDoc(collection(db, "my_devices"), { newItem })
+                console.log(`Document is written to the db`)
+                // 
+                navigate('/')
+            } catch (error) {
+                console.log(`Error occured - ${error}`)
+            }
         }
     }
 
@@ -63,6 +90,15 @@ const Register: React.FC = () => {
 
             <button onClick={handleSubmit}>Register</button>
 
+            {error.isError ? (
+                <>
+                    <div>
+                        <small style={{ color: 'red' }}>{error.error}</small>
+                    </div>
+                </>
+            ) :
+                null
+            }
         </>
     )
 }
