4f1b5040ab95af6d46eb35ebd56bf5fa1a1ed659
vihanpereraux <vihanpereraux@gmail.com>
2025-01-19 08:46:22 +0000

    add chat screen sketch

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,9 +1,27 @@
 import React from "react";
 
+// mui
+import { Button } from "@mui/material";
+
+// import stylesheet
+import Styles from './ChatRoom.module.css';
+
 const ChatRoom: React.FC = () => {
-    return(
+    return (
         <>
-            Chat Room
+            <div className={Styles._wrapper}>
+                <div className={Styles._heading}>
+                    <h3>Public Chat Room</h3>
+                </div>
+                <div className={Styles._chat_space}>
+                    lolx
+                </div>
+                <div className={Styles._message_box}>
+                    <input placeholder="Type your message here " />      
+                    <Button variant="contained">Send</Button>
+                </div>
+            </div>
+            {/* Chat Room */}
         </>
     )
 }
